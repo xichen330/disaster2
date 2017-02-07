fb3dadcccf859f598eddab1f4d0ac26d8c4ddc99
xichen330 <xichen330@gmail.com>
2017-02-07 20:16:22 +0800

    add event

--- today/db/migrate/20170207025038_create_events.rb
+++ today/db/migrate/20170207025038_create_events.rb
@@ -1,10 +1,9 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :events do |t|
-      t.string :name
+      t.integer :category
+      t.string :title
       t.text :description
-      t.boolean :is_public
-      t.integer :capacity
 
       t.timestamps
     end
--- today/db/schema.rb
+++ today/db/schema.rb
@@ -13,10 +13,9 @@
 ActiveRecord::Schema.define(version: 20170207025038) do
 
   create_table "events", force: :cascade do |t|
-    t.string   "name"
+    t.integer  "category"
+    t.string   "title"
     t.text     "description"
-    t.boolean  "is_public"
-    t.integer  "capacity"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
